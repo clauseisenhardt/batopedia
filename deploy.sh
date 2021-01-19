f9ba32a3087f47f7dbc9d48b55ef8ac8f97ef67d
Chewbacca <chewbacca@milliniumfalcon.com>
2021-01-19 10:27:43 +0100

    add install and deploy files

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+# Deploy angular project on firebase hosting
+# Build angualar project
+ng build --prod
+
+# Deploy it
+firebase deploy
+# Check it in browser on: https://batopidia.web.app
---
+++ install.sh
@@ -0,0 +1,16 @@
+# Install firebase CLI
+npm install -g firebase-tools
+
+# login with your google account
+firebase login
+
+# Setup project
+# cd to agular project and call: firebase init
+#   step to Hosting and press <Space>
+#   Create new project
+#   Type name of project = batopedia
+#   Type public directory = build output = dist/batopedia
+#   Single-page app? = y
+#   GitHub? = n 
+#   overwrite index.html? n 
+firebase init
